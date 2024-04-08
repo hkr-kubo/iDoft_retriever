e224626dee9a6ff34baf92ffb660a9d8752f9c52
hkr-kubo <u860243a@ecs.osaka-u.ac.jp>
2024-04-08 16:45:38 +0900

    add csv and python files

---
+++ plot.py
@@ -0,0 +1,35 @@
+import os
+import argparse
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+import difflib
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='Filepath of input folder')
+args = parser.parse_args()
+
+def main():
+    modified_lines = np.zeros(175)
+    files = os.listdir(args.input)
+    for file in files:
+        with open(f"{args.input}{file}","r") as f:
+            modify, addition, deletion = 0, 0, 0
+            for line in f:
+                block = re.search(r"^@@",line)
+                addition += len(re.findall(r"^\+",line))
+                deletion += len(re.findall(r"^-",line))
+                if block != None:
+                    modify += max(addition,deletion)
+                    addition, deletion = 0, 0
+            modify += max(addition,deletion)
+            modified_lines[modify] += 1
+    left = np.arange(175)
+    fig, ax = plt.subplots()
+    plt.bar(left, modified_lines)
+    ax.set_xlabel("Lines of modification")
+    ax.set_ylabel("Number of tests")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
--- retrieve.py
+++ retrieve.py
@@ -1,22 +1,24 @@
 import os
+import subprocess
 import argparse
 import re
 import math
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-
 import asyncio
 import aiohttp
 import traceback
-
 import simplejson as json
+import base64
+from collections import defaultdict
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--github_access_token', help='GitHub access token to overcome API rate limitations')
 parser.add_argument('-i', '--input', help='Filepath of input .csv file containing repo data')
 parser.add_argument('-o', '--output', help='Filepath of output .json file containing information of modified files')
-parser.add_argument('-s', '--semaphore',type=int)
+parser.add_argument('-s', '--semaphore',type=int,default=5)
 args = parser.parse_args()
 
 INPUT, GITHUB_ACCESS_TOKEN = args.input, args.github_access_token
@@ -41,31 +43,90 @@ def make_api_pr_url(pr_url:str):
 async def get_list_of_pr_files(pr_url:str,token:str,session:aiohttp.ClientSession,sem:asyncio.Semaphore):
     if pr_url != None:
         url = make_api_pr_url(pr_url)
+    else:
+        return None
     headers = {"Authorization": f"token {token}"}
     async with sem:
         async with session.get(url,headers=headers) as res:
-            result =  await res.json()
-            return url,result
+            result = await res.json()
+            return result
+        
+async def get_content(content_url:str,token:str,session:aiohttp.ClientSession,sem:asyncio.Semaphore):
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"token {token}"}
+    async with sem:
+        async with session.get(content_url,headers=headers) as res:
+            result = json.loads(await res.text())
+            decoded_result = base64.b64decode(result["content"]).decode("utf-8")
+            return decoded_result
+        
+async def get_diff(pr_url:str,files:list,token:str,session:aiohttp.ClientSession,sem:asyncio.Semaphore):
+    list_of_pr_files = await get_list_of_pr_files(pr_url,token,session,sem)
+    #print(list_of_pr_files)
+    content_list = []
+    for i,pr_files in enumerate(list_of_pr_files):
+        filepath = pr_files["filename"]
+        filename = path_to_file_name(filepath)
+        print(filename,files)
+        if filename in files:
+            content = {}
+            content["name"] = filename
+            content["content"] = await get_content(pr_files["contents_url"],token,session,sem)
+            content["patch"] = pr_files["patch"]
+            content_list.append(content)
+    return content_list
+
+def path_to_file_name(path:str):
+    pattern=r"^.+/([^/]+?)$"
+    result = re.match(pattern,path)
+    return f"{result.group(1)}"
+
+def get_test_file_name(fqn:str):
+    if re.search("::",fqn) != None:
+        pattern=r"^.+/([^/]+\.py).*$"
+        result = re.match(pattern,fqn)
+        try:
+            return result.group(1)
+        except:
+            print(fqn)
+            return None
+    else:
+        pattern=r"^(.+\.)?([^\.]+)\.[^\.]*$"
+        result = re.match(pattern,fqn)
+        try:
+            return f"{result.group(2)}.java"
+        except:
+            print(fqn)
+            return None
 
 async def main():
     df = pd.read_csv(INPUT)
-    prlink = df.PRLink.values
+    prlinks = df.PRLink.values
+    testfiles = df.FullyQualifiedTestName.values if "FullyQualifiedTestName" in df else df.PytestTestName.values if "PytestTestName" in df else None
     urls = set()
     tasks = []
+    files = defaultdict(list[str])
     sem = asyncio.Semaphore(args.semaphore)
     async with aiohttp.ClientSession() as session:
         for idx in range(df.shape[0]):
-            url = prlink[idx]
+            url = prlinks[idx]
+            testfile = get_test_file_name(testfiles[idx])
+            if testfile == None:
+                continue
             if type(url) != float:
                 urls.add(url)
+                files[url].append(testfile)
         for url in urls:
-            tasks.append(get_list_of_pr_files(url,GITHUB_ACCESS_TOKEN,session,sem))
-        for f in tqdm(asyncio.as_completed(tasks,timeout=60.0),total=len(tasks)):
+            tasks.append(get_diff(url,files[url],GITHUB_ACCESS_TOKEN,session,sem))
+        for f in tqdm(asyncio.as_completed(tasks),total=len(tasks)):
             try:
-                result = await f                    
-                if result != None:
-                    with open("./files/test.json",mode="w") as f:
-                        json.dump(result,f)
+                results = await f 
+                for i,result in enumerate(results):
+                    with open(f"./.files/{result["name"]}",mode="w") as f:
+                        f.write(result["content"])
+                    shutil.copy(f"./.files/{result["name"]}",f"./.files/{result["name"]}.orig")    
+                    with open(f"./.patches/{result["name"]}.patch",mode="w") as f:
+                        f.write(result["patch"])
+                    subprocess.run(["patch","-R",f"./.files/{result["name"]}",f"./.patches/{result["name"]}.patch"])#TODO
             except:
                 traceback.print_exc()
 
